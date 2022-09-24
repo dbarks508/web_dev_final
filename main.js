abda7f5b4b05facaaf03fb77e6cd9b302432e861
Dylan Barks <dylan.barks95@gmail.com>
2022-09-23 22:50:37 -0700

    linked css file to main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/views'));
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
