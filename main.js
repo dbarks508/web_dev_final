2f20c854f0fe9e546165152ad2310d23069d9934
Dylan Barks <dylan.barks95@gmail.com>
2022-09-27 13:36:45 -0700

    implimented ejs in courses.ejs

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
+const { render } = require('ejs');
+const mongoose = require('mongoose')
 
 app.use(express.static(__dirname + '/views'));
 
@@ -20,6 +22,13 @@ app.get('/signup', (req, res) => {
 });
 
 app.get('/courses', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/courses.html'));
+    // res.sendFile(path.join(__dirname + '/views/courses.html'));
+    const courses = [
+        {subject: 'English Composition', credits: '4 Credit hours'},
+        {subject: 'Algebra', credits: '4 credit hours'},
+        {subject: 'Astronomy', credits: '3 credit hours'}
+    ];
+
+    res.render('courses', { title: 'course list', courses} );
 });
 
