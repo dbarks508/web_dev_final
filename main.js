cdec7ae9098e5ba72866a8dcdae9ad9dd6fe9500
Dylan Barks <dylan.barks95@gmail.com>
2022-09-27 14:01:49 -0700

    added dbURI for database

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ const mongoose = require('mongoose')
 
 app.use(express.static(__dirname + '/views'));
 
-app.listen(3000);
+const dbURI = 'mongodb+srv://dylanbarks:Cooliest7!@cluster0.zyumiwp.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .then(console.log('connected to db'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/index.html'));
