d7c47a098fdb49e75be2d8fefef156db918c6261
Dylan Barks <dylan.barks95@gmail.com>
2022-09-29 10:02:53 -0700

    added yoga to database

--- main.js
+++ main.js
@@ -11,8 +11,23 @@ app.use(express.static(__dirname + '/views'));
 // const dbURI = 'mongodb+srv://dylanbarks:Cooliest7!@cluster0.zyumiwp.mongodb.net/?retryWrites=true&w=majority';
 const dbURI = 'mongodb+srv://group:webdevfinal@cluster0.xgizzyz.mongodb.net/school-website?retryWrites=true&w=majority'
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => console.log('connected to db'))
     .then((result) => app.listen(3000))
-    .then(console.log('connected to db'));
+    .catch((err) => console.log(err));
+
+// sandbox route test
+app.get('/add-course', (req, res) => {
+    const course = new Course({
+        subject: 'Yoga',
+        credits: 2,
+        description: 'strecthing and breathing'
+    });
+
+    course.save()
+    .then((result) => {
+        res.send(result);
+    });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/index.html'));
