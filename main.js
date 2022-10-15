176196f3549e4ce872404f4d8dd9c75754a9a0e8
Dylan Barks <dylan.barks95@gmail.com>
2022-10-15 08:27:49 -0700

    claned up code

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ const path = require('path');
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
-const { render } = require('ejs');
-const mongoose = require('mongoose');
-const Course = require('./models/course');
-const User = require('./models/User');
-const Cart = require('./models/cart')
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
+
+const { render }      = require('ejs');
+const mongoose        = require('mongoose');
+const Course          = require('./models/course');
+const User            = require('./models/User');
+const Cart            = require('./models/cart')
+const cookieParser    = require('cookie-parser');
+const jwt             = require('jsonwebtoken');
 const { requireAuth } = require('./middleware/authMiddleware');
 
 app.use(express.static(__dirname + '/views'));
@@ -24,29 +25,6 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => console.log('connected to db'))
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
-
-// sandbox routes
-app.get('/add-course', (req, res) => {
-    const course = new Course({
-        subject: 'Spanish',
-        credits: 3,
-        description: 'learn a new language'
-    });
-
-    course.save()
-    .then((result) => {
-        res.send(result);
-    });
-});
-
-app.get('/all-courses', (req, res) =>{
-    Course.find()
-    .then((result) => {
-        res.send(result);
-    });
-});
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // routes
 app.get('/', (req, res) => {
